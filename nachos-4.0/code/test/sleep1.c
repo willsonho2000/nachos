ebe2f7e1ec2b7cf563092f34ebbc921576009a81
myho@asiaa.sinica.edu.tw <myho@asiaa.sinica.edu.tw>
2023-11-22 17:25:18 +0800

    debug

--- nachos-4.0/code/test/sleep1.c
+++ nachos-4.0/code/test/sleep1.c
@@ -3,7 +3,7 @@ main()
 	{
 		int	n;
 		for (n = 0; n < 15; n++) {
-            Sleep( 10000 )
+            Sleep( 10000 );
 			PrintInt(111);
         }
 	}
--- nachos-4.0/code/test/sleep2.c
+++ nachos-4.0/code/test/sleep2.c
@@ -3,7 +3,7 @@ main()
 	{
 		int	n;
 		for (n = 0; n < 5; n++) {
-            Sleep( 30000 )
+            Sleep( 30000 );
 			PrintInt(222);
         }
 	}
