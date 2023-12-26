8f1e82cc5ecd61aedaea5262d31ffdd6ebdee816
myho@asiaa.sinica.edu.tw <myho@asiaa.sinica.edu.tw>
2023-12-26 15:56:45 +0800

    Project3: Part2 files

---
+++ project3/nachos-4.0/code/test/test1.c
@@ -0,0 +1,8 @@
+#include "syscall.h"
+main()
+ 	{
+		int	n;
+ 		for (n=9;n>5;n--)
+ 			PrintInt(n);
+ 	}
+
---
+++ project3/nachos-4.0/code/test/test_sleep2.c
@@ -0,0 +1,13 @@
+#include "syscall.h"
+
+main()
+{
+	int i;
+
+	for (i = 0; i <= 8; i+=2) {
+		Sleep(301);
+		PrintInt(i);
+	}
+}
+
+
