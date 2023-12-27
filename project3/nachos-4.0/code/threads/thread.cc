56f6fe18193a93f274e753546a61477162a1db12
myho@asiaa.sinica.edu.tw <myho@asiaa.sinica.edu.tw>
2023-12-27 21:12:21 +0800

    small modification

--- project3/nachos-4.0/code/threads/thread.cc
+++ project3/nachos-4.0/code/threads/thread.cc
@@ -152,7 +152,7 @@ Thread::Begin ()
     ASSERT(this == kernel->currentThread);
     DEBUG(dbgThread, "Beginning thread: " << name);
     
-    kernel->scheduler->Print();
+    // kernel->scheduler->Print();
     kernel->scheduler->CheckToBeDestroyed();
     kernel->interrupt->Enable();
 }
