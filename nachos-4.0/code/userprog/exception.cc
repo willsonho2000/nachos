29aa01a0070e1ebaeb2c9d097147d3e90b3db4a7
myho@asiaa.sinica.edu.tw <myho@asiaa.sinica.edu.tw>
2023-12-29 19:38:08 +0800

    bug

--- nachos-4.0/code/userprog/exception.cc
+++ nachos-4.0/code/userprog/exception.cc
@@ -142,7 +142,7 @@ ExceptionHandler(ExceptionType which)
 			DEBUG(dbgAddr, "Program exit\n");
 			val=kernel->machine->ReadRegister(4);
 			cout << "return value: " << val << endl;
-			delete kernel->currentThread->space; 	// don't run SaveState()
+			kernel->currentThread->space = NULL; 	// don't run SaveState()
 			kernel->currentThread->Finish();
 			break;
 		default:
